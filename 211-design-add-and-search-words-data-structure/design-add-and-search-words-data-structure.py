e865bf88b406a7235e3dc114ca208f363301f3af
Shabeeha Kalathumpadiyil <139266524+Shabeehak@users.noreply.github.com>
2026-01-28 16:52:59 +0400

    Time: 1060 ms (98.27%) | Memory: 62.6 MB (91.22%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -14,27 +14,26 @@ class WordDictionary(object):
                 node[char]={}
             node = node[char]
         node['#'] = None
+
     def search(self, word):
         """
         :type word: str
         :rtype: bool
         """
-        def dfs(node, i):
-            if i == len(word):
+        def dfs(node, level):
+            if len(word)==level:
                 return '#' in node
-            char = word[i]
-            if char=='.':
+            char = word[level]
+            if char == '.':
                 for child in node:
                     if child!='#':
-                        if dfs(node[child], i+1):
+                        if dfs(node[child], level+1):
                             return True
                 return False
             if char not in node:
                 return False
-            return dfs(node[char], i+1)
+            return dfs(node[char], level+1)
         return dfs(self.root, 0)
-
-
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
 # obj.addWord(word)
