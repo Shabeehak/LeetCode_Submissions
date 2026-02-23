f8de07cbf6ac622eb5cf51a661fa514e7d8b722e
Shabeeha Kalathumpadiyil <139266524+Shabeehak@users.noreply.github.com>
2026-02-23 22:36:50 +0400

    Time: 0 ms (100.00%) | Memory: 12.6 MB (93.23%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -15,7 +15,7 @@ class Solution(object):
         def isMirror(tree1, tree2):
             if not tree1 or not tree2:
                 return tree1==tree2
-            if tree1.val != tree2.val:
+            if tree1.val!=tree2.val:
                 return False
             return isMirror(tree1.left, tree2.right) and isMirror(tree1.right, tree2.left)
         return isMirror(root.left, root.right)
