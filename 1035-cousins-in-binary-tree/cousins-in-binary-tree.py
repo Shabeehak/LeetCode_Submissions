9a2271b5cbbbff5247b85ee2fff5db927dfc69ed
Shabeeha Kalathumpadiyil <139266524+Shabeehak@users.noreply.github.com>
2026-02-24 16:24:10 +0400

    Time: 1 ms (23.74%) | Memory: 12.7 MB (2.88%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
@@ -16,14 +16,13 @@ class Solution(object):
         def dfs(node, parent, depth):
             if not node:
                 return
-            if node.val == x or node.val == y:
-                info[node.val] = (parent, depth)
-            dfs(node.left, node, depth+1) 
+            if node.val==x or node.val==y:
+                info[node.val]=(parent, depth)
+            dfs(node.left, node, depth+1)
             dfs(node.right, node, depth+1)
-
         dfs(root, None, 0)
-
         x_parent, x_depth = info[x]
         y_parent, y_depth = info[y]
-
-        return x_parent!=y_parent and x_depth == y_depth
+        return x_parent!=y_parent and x_depth==y_depth
+        
+        
